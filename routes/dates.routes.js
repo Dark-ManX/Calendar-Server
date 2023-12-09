e3bc979d0c5b98faabd8fdeaa1f627619cad4c47
Serhii <sergiosis@ukr.net>
2023-12-09 03:51:35 +0200

    try to deploy

---
+++ routes/dates.routes.js
@@ -0,0 +1,10 @@
+const Router = require("express");
+const datesController = require("../api/controller/dates.controller");
+
+const { getAllEvents, createEvent, deleteEvent } = datesController;
+const router = new Router();
+router.get("/", getAllEvents);
+router.post("/", createEvent);
+router.delete("/:event", deleteEvent);
+
+module.exports = router;
